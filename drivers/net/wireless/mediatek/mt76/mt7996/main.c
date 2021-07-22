f65c1089d15dc3f472928ef9e78d9c2d73257b2c
Ben Greear <greearb@candelatech.com>
2021-07-22 09:54:48 -0700

    wifi: mt76: mt7996: support enabling rx group-5 status
    When enabled, this allows per-skb rx rate reporting.
    Enabling this may degrade RX performance, so it remains
    disabled by default.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -494,7 +494,7 @@ static void mt7996_set_monitor(struct mt7996_phy *phy, bool enabled)
 		phy->rxfilter &= ~MT_WF_RFCR_DROP_OTHER_UC;
 
 	mt76_rmw_field(dev, MT_DMA_DCR0(phy->mt76->band_idx),
-		       MT_DMA_DCR0_RXD_G5_EN, enabled);
+		       MT_DMA_DCR0_RXD_G5_EN, enabled || dev->rx_group_5_enable);
 	mt7996_phy_set_rxfilter(phy);
 	mt7996_mcu_set_sniffer_mode(phy, enabled);
 }
@@ -522,6 +522,9 @@ static int mt7996_add_interface(struct ieee80211_hw *hw,
 
 		if (vif->type == NL80211_IFTYPE_MONITOR)
 			mt7996_set_monitor(phy, true);
+		else if (dev->rx_group_5_enable)
+			mt76_rmw_field(dev, MT_DMA_DCR0(phy->mt76->band_idx),
+				       MT_DMA_DCR0_RXD_G5_EN, true);
 	}
 
 	mt76_vif_init(vif, &mvif->mt76);
