825dbcee42e9b5b3e93cd043ca3771420f5d3a90
MeiChia Chiu <meichia.chiu@mediatek.com>
2022-06-22 10:45:53 +0800

    mt76: mt7915: add support for muru_onoff via debugfs

--- drivers/net/wireless/mediatek/mt76/mt7915/mcu.h
+++ drivers/net/wireless/mediatek/mt76/mt7915/mcu.h
@@ -540,4 +540,10 @@ enum {
 					 sizeof(struct bss_info_bmc_rate) +\
 					 sizeof(struct bss_info_ext_bss))
 
+/* MURU */
+#define OFDMA_DL                       BIT(0)
+#define OFDMA_UL                       BIT(1)
+#define MUMIMO_DL                      BIT(2)
+#define MUMIMO_UL                      BIT(3)
+
 #endif
