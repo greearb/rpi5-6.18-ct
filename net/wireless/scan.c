0c9f149f3cb3c11b3d2e189e9973bea295357bff
Ben Greear <greearb@candelatech.com>
2023-10-05 10:11:46 -0700

    wifi: Improve debugging around scan failures.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -1058,8 +1058,8 @@ skip:
 		if (err) {
 			rdev->int_scan_req = old;
 			kfree(request);
-			pr_err("cfg80211-scan-6ghz, rdev_scan failed: %d  n_channels: %d  need_scan_psc: %d",
-			       err, request->req.n_channels, need_scan_psc);
+			pr_err("cfg80211-scan-6ghz, rdev_scan failed: %d  n_channels: %d  need_scan_psc: %d req-n-channels: %d",
+			       err, request->req.n_channels, need_scan_psc, rdev_req->req.n_channels);
 		} else {
 			kfree(old);
 		}
@@ -1067,8 +1067,8 @@ skip:
 		return err;
 	}
 
-	pr_err("cfg80211-scan-6ghz, n-channels is 0, request->flags: 0x%x  need_scan_psc: %d\n",
-	       request->req.flags, need_scan_psc);
+	pr_err("cfg80211-scan-6ghz, n-channels is 0, flags: 0x%x  need_scan_psc: %d req-n-channels: %d\n",
+	       request->req.flags, need_scan_psc, rdev_req->req.n_channels);
 	kfree(request);
 	return -EINVAL;
 }
