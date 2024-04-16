f31a77484a9e710d08a16db7a87c70a086de682a
Ben Greear <greearb@candelatech.com>
2024-04-16 16:45:29 -0700

    mt76: Fix a compile error when TESTMODE is not selected.
    There are more problems, however.  I just enabled TESTMODE
    to fix the compile.

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -1849,6 +1849,8 @@ mt76_testmode_param_present(struct mt76_testmode_data *td, u16 idx)
 {
 #ifdef CONFIG_NL80211_TESTMODE
 	return td->param_set[idx / 32] & BIT(idx % 32);
+#else
+	return false;
 #endif
 }
 
