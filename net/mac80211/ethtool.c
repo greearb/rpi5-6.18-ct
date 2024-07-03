2c1bdf123b52190f5d73d6215a18f2bab983a5b0
Ben Greear <greearb@candelatech.com>
2024-07-03 05:08:41 -0700

    wifi: mac80211: ethtool stats whitespace change
    Put each string on its own line to make it easier
    to read.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/ethtool.c
+++ net/mac80211/ethtool.c
@@ -44,13 +44,28 @@ static void ieee80211_get_ringparam(struct net_device *dev,
 }
 
 static const char ieee80211_gstrings_sta_stats[][ETH_GSTRING_LEN] = {
-	"rx_packets", "rx_bytes",
-	"rx_duplicates", "rx_fragments", "rx_dropped",
-	"tx_packets", "tx_bytes",
-	"tx_filtered", "tx_retry_failed", "tx_retries",
-	"tx_handlers_drop", "sta_state", "txrate", "rxrate",
-	"signal", "channel", "noise", "ch_time", "ch_time_busy",
-	"ch_time_ext_busy", "ch_time_rx", "ch_time_tx"
+	"rx_packets",
+	"rx_bytes",
+	"rx_duplicates",
+	"rx_fragments",
+	"rx_dropped",
+	"tx_packets",
+	"tx_bytes",
+	"tx_filtered",
+	"tx_retry_failed",
+	"tx_retries",
+	"tx_handlers_drop",
+	"sta_state",
+	"txrate",
+	"rxrate",
+	"signal",
+	"channel",
+	"noise",
+	"ch_time",
+	"ch_time_busy",
+	"ch_time_ext_busy",
+	"ch_time_rx",
+	"ch_time_tx",
 };
 #define STA_STATS_LEN	ARRAY_SIZE(ieee80211_gstrings_sta_stats)
 
