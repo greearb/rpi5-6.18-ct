5ee3ad13a68eb5ac7d6341c91db00431a59747d0
StanleyYP Wang <StanleyYP.Wang@mediatek.com>
2025-02-17 15:55:52 +0800

    mtk: mt76: mt7996: add 5T5R support for testmode RX
    add 5T5R support for testmode RX (especially for mt7992 BE7200 ifem)
    
    Signed-off-by: StanleyYP Wang <StanleyYP.Wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/mt7996.h
+++ drivers/net/wireless/mediatek/mt76/mt7996/mt7996.h
@@ -450,10 +450,10 @@ struct mt7996_phy {
 		u32 *reg_backup;
 
 		s32 last_freq_offset;
-		u8 last_rcpi[4];
-		s8 last_rssi[4];
-		s8 last_ib_rssi[4];
-		s8 last_wb_rssi[4];
+		u8 last_rcpi[5];
+		s8 last_rssi[5];
+		s8 last_ib_rssi[5];
+		s8 last_wb_rssi[5];
 		u8 last_snr;
 
 		u8 spe_idx;
