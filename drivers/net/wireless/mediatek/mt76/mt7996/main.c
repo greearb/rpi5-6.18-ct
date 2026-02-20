b22752431c6223c6d478e5afe5c8814580b22273
Dylan Eskew <dylan.eskew@candelatech.com>
2026-02-20 09:35:51 -0800

    wifi: mt76: mt7996: fix link_rate_ctrl_update()
    The dev dereference will sometimes fail here, so use the more reliable
    helper mt7996_vif_to_dev().
    
    Signed-off-by: Dylan Eskew <dylan.eskew@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -1757,7 +1757,7 @@ static void mt7996_link_rate_ctrl_update(void *data,
 					 struct mt7996_sta_link *msta_link)
 {
 	struct mt7996_sta *msta = msta_link->sta;
-	struct mt7996_dev *dev = msta->vif->deflink.phy->dev;
+	struct mt7996_dev *dev = mt7996_vif_to_dev(msta->vif);
 	u32 *changed = data;
 
 	spin_lock_bh(&dev->mt76.sta_poll_lock);
